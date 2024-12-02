ad76a0ae2eec43e45923b2d924fafdd77c34c5e9
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-12-02 08:58:29 +0530

    qpo

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -4,7 +4,7 @@ const handleError = (err, req, res, next) => {
     console.error(err.stack); // Log the error stack for debugging
     res.status(err.status || 500).json({
         message: err.message || 'Internal Server Error',
-        ...(process.env.NODE_ENV === 'development' && { stack: err.stack }) // Include stack trace in development
+        ...(process.env.NODE_ENV === 'development' && { stack: err.stack }) 
     });
 };
 
